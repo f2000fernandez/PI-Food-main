c6e0425850911b4cc8a1db8dde8a31daa9d2fa6d
f2000fernandez <f2000.fernandez@gmail.com>
2022-02-03 13:59:50 -0300

    css

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -5,7 +5,7 @@ import './estilos/Landing.css';
 const Landing = () => {
     return (
         <div className='landing'>
-            <p className='wecolme'>Welcome to the Henry food app</p>
+            <p className='welcome'>Welcome to the Henry food app</p>
             <p className='dev'>developed by: Francisco Fernandez</p>
             <Link to="/recipes">
                 <button type='button'> GET STARTED! </button>
